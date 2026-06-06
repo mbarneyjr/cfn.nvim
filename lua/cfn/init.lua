701ebd58aec33212994cc9de727de6c34b2ae6d9
Michael Barney, Jr <mbarneyme@gmail.com>
2026-06-05 21:27:01 -0400

    feat(lua): expose encryption_key method from root cfn module

--- lua/cfn/init.lua
+++ lua/cfn/init.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 local config = require("cfn.lib.config")
+local state = require("cfn.lib.state")
 local commands = require("cfn.commands")
 
 ---@param opts? cfn.SetupOpts
@@ -8,5 +9,7 @@ function M.setup(opts)
   config.setup(opts)
   commands.register()
 end
+
+M.encryption_key = state.encryption_key
 
 return M

21ee7e71cfa73a4a4014466bcedd524c49e23e6f
Michael Barney, Jr <mbarneyme@gmail.com>
2026-08-19 10:10:04 -0400

    fix(buffer): notify if template path has no lsp attached

--- lua/cfn/lib/buffer.lua
+++ lua/cfn/lib/buffer.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+local config = require("cfn.lib.config")
+
 ---@param bufnr? number
 ---@return string? err
 ---@return string? path
@@ -10,6 +12,9 @@ function M.get_current_buffer_template_path(bufnr)
   local filetype = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
   if string.find(filetype, "yaml") == nil and string.find(filetype, "json") == nil then
     return "buffer is not a yaml or json file", nil
+  end
+  if #vim.lsp.get_clients({ bufnr = bufnr, name = config.options.lsp_client_name }) == 0 then
+    return "LSP client '" .. config.options.lsp_client_name .. "' is not attached"
   end
   local template_path = vim.api.nvim_buf_get_name(bufnr)
   if template_path == "" then
